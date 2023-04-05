a359e659fafe779bbc16329e13ba22a5ce856774
isambarboza <129955004+isambarboza@users.noreply.github.com>
2023-04-05 12:06:56 -0300

    soma

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ document.addEventListener("click" , showcolor );
 
 document.getElementById("botao").addEventListener("click", exibenome);
 
+document.getElementById("btn-soma").addEventListener("click" , soma)
 function showcolor()
 {
     document.body.style.backgroundColor = "red";
@@ -12,4 +13,11 @@ function exibenome()
     let usuario = document.getElementById( "nome" ).value;
     document.getElementById( "mensagem" ).innerHTML = "Meu nome é:" + usuario;
     document.getElementById("nome").value = "";
+}
+
+function soma()
+{
+    let numero1 = document.getElementById("num1").value;
+    let numero2 = document.getElementById("num2").value;
+    document.getElementById("resultado1").innerHTML = parseFloat(numero1) + parseFloat(numero2);
 }
