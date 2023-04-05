f7bcc83296241b2fa98734646a29553a19be7aeb
isambarboza <129955004+isambarboza@users.noreply.github.com>
2023-04-05 12:21:57 -0300

    divisao

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ document.addEventListener("click" , showcolor );
 document.getElementById("botao").addEventListener("click", exibenome);
 
 document.getElementById("btn-soma").addEventListener("click" , soma);
+
+document.getElementById("btn-divisao").addEventListener("click" , divisao);
 function showcolor()
 {
     document.body.style.backgroundColor = "red";
@@ -20,4 +22,10 @@ function soma()
     let numero1 = document.getElementById("num1").value;
     let numero2 = document.getElementById("num2").value;
     document.getElementById("resultado1").innerHTML = parseFloat(numero1) + parseFloat(numero2);
+}
+function divisao()
+{
+    let numero3 = document.getElementById("num3").value;
+    let numero4 = document.getElementById("num4").value;
+    document.getElementById("resultado2").innerHTML = parseFloat(numero3) / parseFloat(numero4);
 }
