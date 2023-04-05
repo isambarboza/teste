305c8350109f1424e54b3f07308d7624ce5cbd2f
isambarboza <129955004+isambarboza@users.noreply.github.com>
2023-04-05 11:15:51 -0300

    usuario

--- main.js
+++ main.js
@@ -9,6 +9,6 @@ function showcolor()
 }
 function exibenome()
 {
-let usuario = document.getElementById("nome").value;
-document.getElementById("mensagem").innerText = "Meu nome é:" + usuario;
+    let usuario = document.getElementById( "nome" ).value;
+    document.getElementById( "mensagem" ).innerText = "Meu nome é: " + usuario;
 }
