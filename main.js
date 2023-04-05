2e96cd9b68877a4698c602d107a8ebcfc6302c08
isambarboza <129955004+isambarboza@users.noreply.github.com>
2023-04-05 12:38:22 -0300

    subtrair

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ document.getElementById("btn-divisao").addEventListener("click" , divisao);
 document.getElementById("btn-multiplicar").addEventListener("click" , multiplicar);
 
 document.getElementById("btn-subtrair").addEventListener("click" , subtrair);
+
 function showcolor()
 {
     document.body.style.backgroundColor = "red";
@@ -39,7 +40,7 @@ function multiplicar()
     let numero6 = document.getElementById("num6").value;
     document.getElementById("resultado3").innerHTML = parseFloat(numero5) * parseFloat(numero6);
 }
-function subtracao()
+function subtrair()
 {
     let numero7 = document.getElementById("num7").value;
     let numero8 = document.getElementById("num8").value;
